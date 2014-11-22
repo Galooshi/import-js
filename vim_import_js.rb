3c9931c2ef0158f54f5608ac19336d371af51abd
Henric Trotzig <henric.trotzig@causes.com>
2014-11-22 17:40:04 +0100

    First rudimentary POC version
    I haven't yet made it Pathogen compatible. Heck, it's not even a vim
    plugin yet. But it works, at least for a very basic requirement. Here's
    how to use it:
    
      In vim, place your cursor on a variable to require. Here's a jsx
      example:
    
         <Button/>
    
      Make sure that a file components/button.js.jsx exists.
    
      Type
        :rubyfile <path_to:vim_import_js.rb>
    
    Notice how this gets added to the top of the buffer:
    
      var Button = require('components/button');
    
    Simple as that. Next up:
      - Only add imports that don't already exist.
      - Handle ambiguous cases
      - Make it a vim plugin, Pathogen compatible

---
+++ vim_import_js.rb
@@ -0,0 +1,30 @@
+# Script to automatically import a JS variable
+#
+def camelcase_to_snakecase(string)
+  # Grabbed from
+  # http://stackoverflow.com/questions/1509915/converting-camel-case-to-underscore-case-in-ruby
+  string.gsub(/::/, '/')
+        .gsub(/([A-Z]+)([A-Z][a-z])/, '\1_\2')
+        .gsub(/([a-z\d])([A-Z])/, '\1_\2')
+        .tr('-', '_')
+        .downcase
+end
+
+def find_path_to_file(variable_name)
+  snake_case_variable = camelcase_to_snakecase(variable_name)
+  matched_file_paths = Dir.glob("**/#{snake_case_variable}*.js*")
+
+  # TODO: do something about arrays larger than one
+  return if matched_file_paths.empty?
+  matched_file = matched_file_paths.first
+  matched_file.gsub(/\..*$/, '')
+end
+
+variable_name = VIM.evaluate("expand('<cword>')")
+path_to_file = find_path_to_file(variable_name)
+if path_to_file
+  require_statement = "var #{variable_name} = require('#{path_to_file}');"
+  VIM::Buffer.current.append(0, require_statement)
+else
+  VIM.message("No file found for #{variable_name}")
+end
