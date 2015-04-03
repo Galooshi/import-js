70a27643a8ae0472f8439531c844d54225892aff
Henric Trotzig <henric.trotzig@causes.com>
2015-04-03 19:11:43 +0200

    Show error if YAML configuration file is found
    This will hopefully help others who have already installed this plugin
    and have created a YAML file called ".importjs".
    
    I decided to add it as early as possible in the code and keep it
    separated from the rest of the config. I didn't add any tests either,
    which was a conscious move. Hopefully this won't be long-lived.

--- ruby/import_js.rb
+++ ruby/import_js.rb
@@ -6,3 +6,13 @@ end
 
 require_relative 'import_js/importer'
 require_relative 'import_js/configuration'
+
+# @deprecated we've moved to JSON configuration, so the old YAML file is no
+# longer supported
+if File.exist?('.importjs')
+  fail <<-EOS.split.join(' ')
+    [import-js] this project is no longer configured with YAML. Please
+    migrate your .importjs file to a JSON file called ".importjs.json"
+    instead.'
+  EOS
+end
