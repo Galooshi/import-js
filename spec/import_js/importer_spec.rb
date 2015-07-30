Galooshi/import-js

commit 1cc2157ff9f0d87a11c9b70a354dec298eea323e

Add support for imports with line-breaks
As reported by @lencioni in #9:

  If I have a section of required modules at the top that looks like this:

    const MyModule = require('path/to/my_module');
    const MyModuleThatHasALongName =
      require('path/to/my_module_that_has_a_long_name');
    const SomeModule = require('path/to/some_module');

  import-js gets confused when adding a new module to the list:

    const AnotherModule = require('path/to/another_module');
    const MyModule = require('path/to/my_module');

    const MyModuleThatHasALongName =
      require('path/to/my_module_that_has_a_long_name');
    const SomeModule = require('path/to/some_module');

  where I would expect:

    const AnotherModule = require('path/to/another_module');
    const MyModule = require('path/to/my_module');
    const MyModuleThatHasALongName =
      require('path/to/my_module_that_has_a_long_name');
    const SomeModule = require('path/to/some_module');

  It would be nice if import-js worked better with this format.

I've added basic support for line breaks in this commit. I say basic
because I expect there to be edge cases that I haven't thought of.
We use simple regexs to parse the js file content, and like all simple
regex parsers, they are bound to fail. However, I don't want to bring in
a js parsing framework just yet. So in this commit I extend the regex
approach with some logic to deal with possible line-breaks.

One idea also listed in #9 was to add support for import-js to
automatically line-break long lines. I haven't made that change in this
commit (new imports are still one-line), but I might do that later if I
find time.

ruby/import-js/importer.rb
@@ -103,42 +103,55 @@ module ImportJS
     # @return [number] the number of lines changed
     def write_imports(variable_name, path_to_file)
       current_imports = find_current_imports
-      before_length = current_imports.length
-      current_imports.length.times do
+
+      unless buffer[current_imports[:newline_count] + 1].strip.empty?
+        # Add a newline after imports
+        buffer.append(current_imports[:newline_count], '')
+      end
+
+      # delete current imports
+      current_imports[:newline_count].times do
         buffer.delete(1)
       end
 
-      declaration_keyword = @config['declaration_keyword']
-      current_imports << "#{declaration_keyword} #{variable_name} = require('#{path_to_file}');"
+      imports = current_imports[:imports]
+      before_length = imports.length
 
-      current_imports.sort!.uniq! do |import|
+      declaration_keyword = @config['declaration_keyword']
+      imports << "#{declaration_keyword} #{variable_name} = require('#{path_to_file}');"
+
+      imports.sort!.uniq! do |import|
         # Determine uniqueness by discarding the declaration keyword (`const`,
-        # `let`, or `var`).
-        import.sub(/\A(const|let|var)\s+/, '')
+        # `let`, or `var`) and normalizing multiple whitespace chars to single
+        # spaces.
+        import.sub(/\A(const|let|var)\s+/, '').sub(/\s\s+/s, ' ')
       end
 
-      current_imports.reverse.each do |import_line|
+      # add imports back in
+      imports.reverse.each do |import_line|
         buffer.append(0, import_line)
       end
 
-      after_length = current_imports.length
-      unless buffer[current_imports.length + 1].strip.empty?
-        # Add a newline after imports
-        buffer.append(current_imports.length, '')
-        after_length += 1
-      end
-      after_length - before_length
+      imports.length - before_length # truthy if the import was new
     end
 
     # @return [Array]
     def find_current_imports
-      lines = []
+      imports_blob = ''
       buffer.count.times do |n|
         line = buffer[n + 1]
-        break unless line.match(/^(const|let|var)\s+.+=\s+require\(.*\).*;\s*$/)
-        lines << line
+        break if line.strip.empty?
+        imports_blob << "\n#{line}"
       end
-      lines
+
+      imports = imports_blob.scan(/(?:const|let|var)\s+.+=\s+require\(.*\).*;/)
+      newline_count = imports.length + imports.reduce(0) do |sum, import|
+        sum + import.scan(/\n/).length
+      end
+      {
+        imports: imports,
+        newline_count: newline_count
+      }
     end
 
     # @param variable_name [String]

spec/import-js/importer_spec.rb
@@ -157,6 +157,27 @@ foo
         end
       end
 
+      context 'when there is an import with line-breaks' do
+        let(:text) { <<-EOS.strip }
+var zoo =
+  require('foo/zoo');
+var tsar = require('foo/bar').tsar;
+
+var foo = { require: b }
+        EOS
+
+        it 'adds the import, sorts the entire list and keeps the line-break' do
+          expect(subject).to eq(<<-EOS.strip)
+var foo = require('bar/foo');
+var tsar = require('foo/bar').tsar;
+var zoo =
+  require('foo/zoo');
+
+var foo = { require: b }
+        EOS
+        end
+      end
+
       context 'when there is a blank line amongst current imports' do
         let(:text) { <<-EOS.strip }
 var zoo = require('foo/zoo');
@@ -305,6 +326,23 @@ foo
             EOS
 
             it 'changes the `var` to declaration_keyword' do
+              expect(subject).to eq(<<-EOS.strip)
+const foo = require('bar/foo');
+
+foo
+              EOS
+            end
+          end
+
+          context 'when the import contains a line-break' do
+            let(:text) { <<-EOS.strip }
+var foo =
+  require('bar/foo');
+
+foo
+            EOS
+
+            it 'changes the `var` to declaration_keyword and removes the whitespace' do
               expect(subject).to eq(<<-EOS.strip)
 const foo = require('bar/foo');
 

commit c61d44a8c3fdf74bcedd986b60d21983d4d9e051

Fix bug with organizing imports
If you had a buffer that looked something like this

  var a = require('a');
  var b = a.b;
  var c = require('c');

the buffer would be changed to this on successful import:

  var a = require('a');
  var c = require('c');

  var b = a.b;
  var c = require('c');

Notice the duplicate `c` import. The bug was probably introduced in
1cc2157 when I added support for multiline imports. I change the code in
find_current_imports to stop scanning for imports if it finds something
that is not an import. The result is that import-js will become better
at breaking off the list of imports from the rest of top-level
initialization, but if you start mixin imports and regular assignment at
the top you might be surprised with the result. For instance, the
example shown above will result in this output (let's pretend that
you're importing `d` here):

  var a = require('a');
  var d = require('d');

  var b = a.b;
  var c = require('c');

This output is partly a result of what was easy to implement, and a
future enhancement might change the implementation to generate this
output instead (again, let's pretend we're importing `d`):

  var a = require('a');
  var c = require('c');
  var d = require('d');

  var b = a.b;

ruby/import_js/importer.rb
@@ -137,14 +137,27 @@ module ImportJS
 
     # @return [Hash]
     def find_current_imports
-      imports_blob = ''
+      potential_import_lines = []
       buffer.count.times do |n|
         line = buffer[n + 1]
         break if line.strip.empty?
-        imports_blob << "\n#{line}"
+        potential_import_lines << line
       end
 
-      imports = imports_blob.scan(/(?:const|let|var)\s+.+=\s+require\(.*\).*;/)
+      # We need to put the potential imports back into a blob in order to scan
+      # for multiline imports
+      potential_imports_blob = potential_import_lines.join("\n")
+
+      imports = []
+
+      # Scan potential imports for everything ending in a semicolon, then
+      # iterate through those and stop at anything that's not an import.
+      potential_imports_blob.scan(/^.*?;/m).each do |potential_import|
+        break unless potential_import.match(
+          /(?:const|let|var)\s+.+=\s+require\(.*\).*;/)
+        imports << potential_import
+      end
+
       newline_count = imports.length + imports.reduce(0) do |sum, import|
         sum + import.scan(/\n/).length
       end

spec/import_js/importer_spec.rb
@@ -220,6 +220,36 @@ foo
         end
       end
 
+      context 'when there is a non-import inline with the imports' do
+        let(:text) { <<-EOS.strip }
+var bar = require('bar');
+var star =
+  require('star');
+var { STRAWBERRY, CHOCOLATE } = bar.scoops;
+var zoo = require('foo/zoo');
+
+foo
+        EOS
+
+        it 'breaks imports at that line' do
+          # A better solution would perhaps be to find the `var zoo` import and
+          # move it up there with the rest. But there's a lot of complexity
+          # involved in that, so cutting off at the non-import is a simpler
+          # solution.
+          expect(subject).to eq(<<-EOS.strip)
+var bar = require('bar');
+var foo = require('bar/foo');
+var star =
+  require('star');
+
+var { STRAWBERRY, CHOCOLATE } = bar.scoops;
+var zoo = require('foo/zoo');
+
+foo
+        EOS
+        end
+      end
+
       context 'when there is an import with line-breaks' do
         let(:text) { <<-EOS.strip }
 var zoo =