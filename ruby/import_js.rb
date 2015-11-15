c02bbe684e589914bad70a6ae717513700622a2c
Henric Trotzig <henric.trotzig@brigade.com>
2015-11-15 22:12:59 +0100

    Remove warning about deprecated .importjs
    This deprecation has been there long enough to serve its purpose. I'm
    cleaning it out to stay lean.

--- ruby/import_js.rb
+++ ruby/import_js.rb
@@ -8,13 +8,3 @@ require_relative 'import_js/js_module'
 require_relative 'import_js/importer'
 require_relative 'import_js/vim_editor'
 require_relative 'import_js/configuration'
-
-# @deprecated we've moved to JSON configuration, so the old YAML file is no
-# longer supported
-if File.exist?('.importjs')
-  fail <<-EOS.split.join(' ')
-    [import-js] this project is no longer configured with YAML. Please
-    migrate your .importjs file to a JSON file called ".importjs.json"
-    instead.'
-  EOS
-end
