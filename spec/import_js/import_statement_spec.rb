4f7f05b26d93ee77826fdfdbef584bab6b2c759c
Henric Trotzig <henric.trotzig@brigade.com>
2015-12-04 11:03:16 +0100

    Consolidate parsing and constructing import statements
    To abstract away the complexities involved in the regexes used to parse
    import statements, I'm introducing a class to handle it. I'm also moving
    import statement construction into this new class, including handling of
    deconstructed imports.
    
    This should reduce the risk for introducing problems further down the
    line, and DRYs up some code.
    
    There is some test overlap between the importer_spec.rb and the new
    import_statement_spec.rb, and we could consider DRYing up that a bit
    later.
    
    Next steps might be to make ImportJS::Importer pass around
    ImportJS::ImportStatement instances instead of strings. But that's for a
    different commit.

---
+++ spec/import_js/import_statement_spec.rb
@@ -0,0 +1,101 @@
+require 'spec_helper'
+
+describe 'ImportStatement' do
+  let(:string) { "const foo = require('foo');" }
+
+  subject { ImportJS::ImportStatement.parse(string) }
+
+  context 'when the string is a valid import using const' do
+    it 'returns a valid ImportStatement instance' do
+      expect(subject.assignment).to eq('foo')
+      expect(subject.path).to eq('foo')
+    end
+
+    context 'and it has line breaks' do
+      let(:string) { "const foo = \n  require('foo');" }
+
+      it 'returns a valid ImportStatement instance' do
+        expect(subject.assignment).to eq('foo')
+        expect(subject.path).to eq('foo')
+      end
+
+      it 'is not destructured' do
+        expect(subject.is_destructured).to be_falsy
+      end
+    end
+
+    context 'and it has a destructured assignment' do
+      let(:string) { "const { foo } = require('foo');" }
+
+      it 'returns a valid ImportStatement instance' do
+        expect(subject.assignment).to eq('{ foo }')
+        expect(subject.path).to eq('foo')
+        expect(subject.is_destructured).to be_truthy
+        expect(subject.variables).to eq(['foo'])
+      end
+
+      context 'injecting a new variable' do
+        let(:injected_variable) { 'bar' }
+        let(:statement) do
+          statement = subject
+          statement.inject_variable(injected_variable)
+          statement
+        end
+
+        it 'adds that variable and sorts the list' do
+          expect(statement.variables).to eq(['bar', 'foo'])
+        end
+
+        it 'can reconstruct using `to_import_string`' do
+          expect(statement.to_import_string('const', 80, ' '))
+            .to eq("const { bar, foo } = require('foo');")
+        end
+
+        context 'injecting a variable that is already in the list' do
+          let(:injected_variable) { 'foo' }
+
+          it 'does not add a duplicate' do
+            expect(statement.variables).to eq(['foo'])
+          end
+        end
+      end
+    end
+  end
+
+  context 'when the string is a valid es6 import' do
+    let(:string) { "import foo from 'foo';" }
+
+    it 'returns a valid ImportStatement instance' do
+      expect(subject.assignment).to eq('foo')
+      expect(subject.path).to eq('foo')
+    end
+
+    context 'and it has line breaks' do
+      let(:string) { "import foo\n  from 'foo';" }
+
+      it 'returns a valid ImportStatement instance' do
+        expect(subject.assignment).to eq('foo')
+        expect(subject.path).to eq('foo')
+      end
+    end
+
+    context 'and it has a destructured assignment' do
+      let(:string) { "import { foo } from 'foo';" }
+
+      it 'returns a valid ImportStatement instance' do
+        expect(subject.assignment).to eq('{ foo }')
+        expect(subject.path).to eq('foo')
+        expect(subject.is_destructured).to be_truthy
+        expect(subject.variables).to eq(['foo'])
+      end
+    end
+  end
+
+  context 'when the string is not a valid import' do
+    let(:string) { 'var foo = bar.hello;' }
+
+    it 'returns nil' do
+      expect(subject).to be_nil
+    end
+  end
+end
