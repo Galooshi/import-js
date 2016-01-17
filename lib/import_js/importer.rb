d60436997d31c6c997e492f4e87b536957e03f32
Henric Trotzig <henric.trotzig@brigade.com>
2016-01-17 13:15:12 +0100

    Add comment explaining alternate plural solution
    Before I saw a suggestion by @lencioni [1] to solve matching plural
    folder names, I had already implemented a solution. To keep some of that
    discussion, and to highlight that the plural matching can be
    problematic, I'm injecting a comment to explain the alternative
    solution.
    
    [1]: https://github.com/trotzig/import-js/issues/127#issuecomment-172249242

--- lib/import_js/importer.rb
+++ lib/import_js/importer.rb
@@ -335,7 +335,16 @@ module ImportJS
       # Based on
       # http://stackoverflow.com/questions/1509915/converting-camel-case-to-underscore-case-in-ruby
 
-      # The pattern to match in between words.
+      # The pattern to match in between words. The "es" and "s" match is there
+      # to catch pluralized folder names. There is a risk that this is overly
+      # aggressive and will lead to trouble down the line. In that case, we can
+      # consider adding a configuration option to control mapping a singular
+      # variable name to a plural folder name (suggested by @lencioni in #127).
+      # E.g.
+      #
+      # {
+      #   "^mock": "./mocks/"
+      # }
       split_pattern = '(?:es|s)?.?'
 
       # Split up the string, allow pluralizing and a single (any) character
