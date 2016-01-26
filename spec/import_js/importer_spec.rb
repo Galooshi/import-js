Galooshi/import-js

commit 23804fecc8ab9f85a4f11cb1942bf2715a292fd5

Abstract timing in Importer
Instead of manually setting up this hash, it seems that this logic
should be delegated to a function. This will make sure that it is done
correctly everywhere we need it.

I originally considered adding start_timing and end_timing functions to
do this, but I decided to go with a single function that takes a block
since I like the semantics better that way.

lib/import_js/importer.rb
@@ -43,10 +43,11 @@ module ImportJS
 
     def goto
       @config = ImportJS::Configuration.new(@editor.path_to_current_file)
-      @timing = { start: Time.now }
-      variable_name = @editor.current_word
-      js_modules = find_js_modules(variable_name)
-      @timing[:end] = Time.now
+      js_modules = []
+      time do
+        variable_name = @editor.current_word
+        js_modules = find_js_modules(variable_name)
+      end
       return if js_modules.empty?
       js_module = resolve_one_js_module(js_modules, variable_name)
       @editor.open_file(js_module.file_path) if js_module
@@ -126,9 +127,10 @@ module ImportJS
     # @param variable_name [String]
     # @return [ImportJS::JSModule?]
     def find_one_js_module(variable_name)
-      @timing = { start: Time.now }
-      js_modules = find_js_modules(variable_name)
-      @timing[:end] = Time.now
+      js_modules = []
+      time do
+        js_modules = find_js_modules(variable_name)
+      end
       if js_modules.empty?
         message(
           "No JS module to import for variable `#{variable_name}` #{timing}")
@@ -408,6 +410,13 @@ module ImportJS
         .gsub(/([a-z\d])([A-Z])/, '\1' + split_pattern + '\2') # camelCase
         .tr('-_', split_pattern)
         .downcase
+    end
+
+    def time
+      timing = { start: Time.now }
+      yield
+      timing[:end] = Time.now
+      @timing = timing
     end
 
     # @return [String]

commit f5df155dcc74640e3c8c6ac179dbd94dc9cc6423

Fix `#goto`
This feature broke in 23804fec. Oddly enough, having the variable_name
assignment inside the `time` block seems to work for some ruby versions.
But the one used by my vim (2.0.0-p481) had problems with it.

One of the reasons this could break so easily was that we didn't have
any tests for it.

lib/import_js/importer.rb
@@ -41,8 +41,8 @@ module ImportJS
     def goto
       @config = ImportJS::Configuration.new(@editor.path_to_current_file)
       js_modules = []
+      variable_name = @editor.current_word
       time do
-        variable_name = @editor.current_word
         js_modules = find_js_modules(variable_name)
       end
       return if js_modules.empty?

spec/import_js/importer_spec.rb
@@ -2231,4 +2231,17 @@ bar
       end
     end
   end
+
+  describe '#goto' do
+    let(:existing_files) { ['bar/foo.jsx'] }
+    subject { described_class.new.goto }
+
+    context 'with a variable name that will resolve' do
+      it 'opens the file' do
+        expect_any_instance_of(ImportJS::VIMEditor).to receive(
+          :open_file).with("#{File.basename(@tmp_dir)}/bar/foo.jsx")
+        subject
+      end
+    end
+  end
 end