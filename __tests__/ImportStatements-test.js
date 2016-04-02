385ff4cb5ee05be11da4db331731f7a1db79a9d2
Joe Lencioni <joe.lencioni@gmail.com>
2016-04-02 10:28:44 -0700

    Enable some ImportStatements tests
    I left a couple of these in a pending state since they aren't working
    correctly because I haven't properly set up the Configuration mocking.

--- __tests__/ImportStatements-test.js
+++ __tests__/ImportStatements-test.js
@@ -1,173 +1,88 @@
 'use strict';
 
-jest.unmock('../lib/ImportStatement');
-jest.unmock('../lib/ImportStatements');
-jest.unmock('lodash.flattendeep');
-jest.unmock('lodash.partition');
-jest.unmock('lodash.sortby');
-jest.unmock('lodash.uniqby');
+jest.autoMockOff();
+jest.mock('../lib/Configuration');
 
 const Configuration = require('../lib/Configuration');
 const ImportStatement = require('../lib/ImportStatement');
 const ImportStatements = require('../lib/ImportStatements');
 
 describe('ImportStatements', () => {
-  //let(:configuration) do
-    //{}
-  //});
+  let statements;
 
-  //beforeEach(() => {
-    //allow_any_instance_of(ImportJS::Configuration)
-      //.to receive(:load_config).and_return(nil)
-    //allow_any_instance_of(ImportJS::Configuration)
-      //.to receive(:load_config).with('.importjs.json')
-      //.and_return(configuration)
-  //});
+  beforeEach(() => {
+    const configuration = new Configuration();
+    statements = new ImportStatements(configuration);
+  });
 
-  //let(:config) { ImportJS::Configuration.new('') }
+  it('gives an empty array without any import statements', () => {
+    expect(statements.toArray()).toEqual([]);
+  });
 
-  //subject { described_class.new(config) }
+  it('returns the pushed import statement', () => {
+    statements.push(ImportStatement.parse("import foo from 'foo';"));
 
-  //it('gives an empty array', () => {
-    //expect(subject.to_a).to eq([])
-  //});
+    expect(statements.toArray()).toEqual([
+      "import foo from 'foo';",
+    ]);
+  });
 
-  //context('when pushed an import statement', () => {
-    //let(:import_statement) do
-      //ImportStatement.parse("import foo from 'foo';")
-    //});
+  it('returns one statement when pushed two identical statements', () => {
+    statements.push(ImportStatement.parse("import foo from 'foo';"));
+    statements.push(ImportStatement.parse("import foo from 'foo';"));
 
-    //beforeEach(() => {
-      //subject << import_statement
-    //});
+    expect(statements.toArray()).toEqual([
+      "import foo from 'foo';",
+    ]);
+  });
 
-    //it('gives an array with an array with the statement', () => {
-      //expect(subject.to_a).to eq(
-        //[
-          //"import foo from 'foo';",
-        //]
-      //)
-    //});
-  //});
+  it('returns sorted in same group when pushed two of the same kind', () => {
+    statements.push(ImportStatement.parse("import foo from 'foo';"));
+    statements.push(ImportStatement.parse("import bar from 'bar';"));
 
-  //context('when pushed two identical import statements', () => {
-    //let(:first_import_statement) do
-      //ImportStatement.parse("import foo from 'foo';")
-    //});
+    expect(statements.toArray()).toEqual([
+      "import bar from 'bar';",
+      "import foo from 'foo';",
+    ]);
+  });
 
-    //let(:second_import_statement) do
-      //ImportStatement.parse("import foo from 'foo';")
-    //});
+  it('merges statements of different kinds with identical paths', () => {
+    statements.push(ImportStatement.parse("import foo from 'foo';"));
+    statements.push(ImportStatement.parse("import { bar } from 'foo';"));
 
-    //beforeEach(() => {
-      //subject << first_import_statement
-      //subject << second_import_statement
-    //});
+    expect(statements.toArray()).toEqual([
+      "import foo, { bar } from 'foo';",
+    ]);
+  });
 
-    //it('gives an array with a single statement', () => {
-      //expect(subject.to_a).to eq(
-        //[
-          //"import foo from 'foo';",
-        //]
-      //)
-    //});
-  //});
+  xit('separates import and const', () => {
+    // TODO make default configuration happen
 
-  //context('when pushed two import statements of the same kind', () => {
-    //let(:import_statements) do
-      //[
-        //ImportStatement.parse("import foo from 'foo';"),
-        //ImportStatement.parse("import bar from 'bar';"),
-      //]
-    //});
+    statements.push(ImportStatement.parse("import foo from 'foo';"));
+    statements.push(ImportStatement.parse("const bar = require('bar');"));
 
-    //beforeEach(() => {
-      //import_statements.each { |import_statement| subject << import_statement }
-    //});
+    expect(statements.toArray()).toEqual([
+      "import foo from 'foo';",
+      '',
+      "const bar = require('bar');",
+    ]);
+  });
 
-    //it('gives an array with the two statements sorted in the same group', () => {
-      //expect(subject.to_a).to eq(
-        //[
-          //"import bar from 'bar';",
-          //"import foo from 'foo';",
-        //]
-      //)
-    //});
+  xdescribe('when group_imports is false', () => {
+    // TODO configure group_imports to be false
 
-    //it('is enumerable', () => {
-      //enumerations = 0
-      //subject.each_with_index do |import_statement, i|
-        //expect(import_statement).to eq(import_statements[i])
-        //enumerations += 1
-      //});
-      //expect(enumerations).to eq(2)
-    //});
-  //});
+    it('does not separate statements of different kinds', () => {
+      statements.push(ImportStatement.parse("import foo from 'foo';"));
+      statements.push(ImportStatement.parse("const bar = require('bar');"));
 
-  //context('when pushed two statements of different kinds but the same path', () => {
-    //let(:first_import_statement) do
-      //ImportStatement.parse("import foo from 'foo';")
-    //});
-
-    //let(:second_import_statement) do
-      //ImportStatement.parse("import { bar } from 'foo';")
-    //});
-
-    //beforeEach(() => {
-      //subject << first_import_statement
-      //subject << second_import_statement
-    //});
-
-    //it('merges the two statements', () => {
-      //expect(subject.to_a).to eq(
-        //[
-          //"import foo, { bar } from 'foo';",
-        //]
-      //)
-    //});
-  //});
+      expect(statements.toArray()).toEqual([
+        "const bar = require('bar');",
+        "import foo from 'foo';",
+      ]);
+    });
+  });
 
   //context('when pushed two import statements of different kinds', () => {
-    //let(:first_import_statement) do
-      //ImportStatement.parse("import foo from 'foo';")
-    //});
-
-    //let(:second_import_statement) do
-      //ImportStatement.parse("const bar = require('bar');")
-    //});
-
-    //beforeEach(() => {
-      //subject << first_import_statement
-      //subject << second_import_statement
-    //});
-
-    //it('gives the two statements in different groups', () => {
-      //expect(subject.to_a).to eq(
-        //[
-          //"import foo from 'foo';",
-          //'',
-          //"const bar = require('bar');",
-        //]
-      //)
-    //});
-
-    //context('when `group_imports` is false', () => {
-      //let(:configuration) do
-        //{
-          //'group_imports' => false,
-        //}
-      //});
-
-      //it('returns a single, ordered group', () => {
-        //expect(subject.to_a).to eq(
-          //[
-            //"const bar = require('bar');",
-            //"import foo from 'foo';",
-          //]
-        //)
-      //});
-    //});
-
     //context('when one statement is a package dependency', () => {
       //let(:second_import_statement) do
         //ImportStatement.parse("import bar from 'bar';")
