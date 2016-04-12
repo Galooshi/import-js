abb0d569bba3e46228c398b28ec7ea7e5991d7ae
Joe Lencioni <joe.lencioni@gmail.com>
2016-04-11 21:27:25 -0700

    Remove Ruby version
    Now that we are well along our way to finishing our conversion to
    JavaScript, I don't think we really need the Ruby version sitting around
    cluttering things up.
    
    I noticed a couple more files that I missed extracting and copied them
    to their respective repos as well.

--- lib/vim_import_js.rb
+++
@@ -1,4 +0,0 @@
-# This is a proxy file for ./import_js.rb. Vim needs it so that we can prevent
-# any installed import_js gems from being loaded before the files in the local
-# filesystem.
-require_relative './import_js'
--- spec/spec_helper.rb
+++
@@ -1,3 +0,0 @@
-# Add `ruby` folder to load path
-$LOAD_PATH << File.join(File.dirname(__FILE__), '../lib')
-require 'import_js'
