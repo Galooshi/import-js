468f4f34bd8f9586a38d51eaa35ff1997dac9d3f
Joe Lencioni <joe.lencioni@gmail.com>
2016-05-14 10:23:47 -0700

    Add Flow
    I'd like to use a static type checker to make our code mode readable and
    to help us find bugs before they happen. It seems that our options are
    currently Flow or TypeScript. I don't have experience with either
    really, so I decided to give Flow a whirl. Since we are already partly
    in the Facebook ecosystem with Jest, it seems like this might be a good
    bet.
    
    This adds some flow annotations to a few of our files, to see how it
    feels. It ended up finding one possible bug already, which is a nice
    sign. If we like the direction this is heading, we can expand it to all
    of our files, and then eventually remove the `// @flow` pragma and just
    have it on automatically.
    
    One somewhat unfortunate downside I ran into was the need to add a Babel
    transform for class properties, since Flow requires annotations on class
    properties and this is the way it achieves that. This is only
    unfortunate because this hasn't been solidified enough in the spec to
    make it to stage 3, so it may end up changing dramatically or never
    making it into the language officially, which might mean that we'll end
    up with some tech debt here. However, I think the tradeoff is worth it
    in this case and since we are already going in on a non-standard Flow
    syntax, it seems like we might as well do this too.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,10 @@
 module.exports = {
+  parser: 'babel-eslint',
+
   plugins: [
+    'flowtype',
+    'flow-vars',
+
     // Although we don't have any React or JSX in this project, we need to have
     // this plugin and some of the rules from this plugin enabled here so that
     // our tests work. We should probably look into a way of doing this so that
@@ -14,5 +19,22 @@ module.exports = {
 
   rules: {
     'no-underscore-dangle': ['error', { allowAfterThis: true }],
-  }
+
+    'flowtype/require-parameter-type': 2,
+    'flowtype/require-return-type': [2, 'always', {
+      annotateUndefined: 'never'
+    }],
+    'flowtype/space-after-type-colon': [2, 'always'],
+    'flowtype/space-before-type-colon': [2, 'never'],
+    'flowtype/type-id-match': [2, "^([A-Z][a-z0-9]+)+Type$"],
+
+    'flow-vars/define-flow-type': 2,
+    'flow-vars/use-flow-type': 2,
+  },
+
+  settings: {
+    flowtype: {
+      onlyFilesWithFlowAnnotation: true,
+    },
+  },
 };
