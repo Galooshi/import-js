f311240228c49243a4a1bed5571e3833d9482669
Joe Lencioni <joe.lencioni@gmail.com>
2016-05-14 08:25:49 -0700

    Replace eslint-config-airbnb with eslint-config-airbnb-base
    This package has been split up into a base package for regular
    JavaScript, and another package that includes everything, including
    React rules. Since we don't actually use React or JSX in this project,
    we can switch to the base package and simplify a little.
    
    One downside is that we have a few tests that depend on the React plugin
    being configured here, so I've kept that around. As I mention in a
    comment, this is weird and it would be nice to scope this to just the
    tests that depend on it, but this will do for now.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,16 @@
 module.exports = {
-  extends: 'airbnb',
+  plugins: [
+    // Although we don't have any React or JSX in this project, we need to have
+    // this plugin and some of the rules from this plugin enabled here so that
+    // our tests work. We should probably look into a way of doing this so that
+    // this configuration is scoped specifically for the tests that require it.
+    'react',
+  ],
+
+  extends: [
+    'airbnb-base',
+    'plugin:react/recommended',
+  ],
 
   rules: {
     'no-underscore-dangle': ['error', { allowAfterThis: true }],
