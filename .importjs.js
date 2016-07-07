f0d06e832b44b0220ba0a953732693c1f9620f0b
Henric Trotzig <henric.trotzig@brigade.com>
2016-07-07 22:10:32 +0200

    Update own .importjs.js config
    Now that local configuration through specifying configuration as an
    array, it's best if our own config is correct.

--- .importjs.js
+++ .importjs.js
@@ -1,19 +1,12 @@
-module.exports = [
-  {
-    environments: ['node'],
-    ignorePackagePrefixes: ['lodash.'],
-    logLevel: 'debug',
-    excludes: [
-      './build/**',
-      './lib/__mocks__/**'
-    ]
-  },
-  {
-    appliesTo: 'lib/__tests__/**',
-    importDevDependencies: true
-  },
-  {
-    appliesTo: 'lib/benchmark.js',
-    importDevDependencies: true
-  }
-]
+module.exports = {
+  environments: ['node'],
+  ignorePackagePrefixes: ['lodash.'],
+  logLevel: 'debug',
+  excludes: [
+    './build/**',
+    './lib/__mocks__/**'
+  ],
+  importDevDependencies: ({ pathToCurrentFile }) =>
+    /lib\/__tests__/.test(pathToCurrentFile) ||
+      /lib\/benchmark\.js/.test(pathToCurrentFile)
+}
