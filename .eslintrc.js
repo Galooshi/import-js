ae256a45bfd8f676939afd96f7f3a209ce1e3aac
Joe Lencioni <joe.lencioni@gmail.com>
2016-08-20 08:18:12 -0700

    Enable new flowtype eslint rules
    There have been some new rules added since we started using this plugin.
    It looks like we are already following them, so no additional cleanup is
    needed.

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,14 +21,19 @@ module.exports = {
     'no-underscore-dangle': ['error', { allowAfterThis: true }],
 
     'flowtype/define-flow-type': 2,
+    'flowtype/generic-spacing': [2, 'never'],
     'flowtype/require-parameter-type': 2,
     'flowtype/require-return-type': [2, 'always', {
       annotateUndefined: 'never'
     }],
+    'flowtype/require-valid-file-annotation': [2, 'always'],
     'flowtype/space-after-type-colon': [2, 'always'],
     'flowtype/space-before-type-colon': [2, 'never'],
+    'flowtype/space-before-generic-bracket': [2, 'never'],
+    'flowtype/union-intersection-spacing': [2, 'always'],
     'flowtype/type-id-match': [2, "^([A-Z][a-z0-9]+)+Type$"],
     'flowtype/use-flow-type': 2,
+    'flowtype/valid-syntax': 2,
   },
 
   settings: {
